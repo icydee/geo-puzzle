2fa3f7e5ca4720e9d4a9bd113cb829d22361460e
Iain C Docherty <github@iain-c-docherty.com>
2018-03-16 20:54:04 +0000

    Tested with output circuit

--- bin/geo-puzzle.pl
+++ bin/geo-puzzle.pl
@@ -9,11 +9,30 @@ use FindBin;
 use lib "$FindBin::Bin/../lib";
 
 use Icydee::GeoPuzzle;
-
+use Icydee::HC595;
+use Time::HiRes qw(gettimeofday tv_interval);
 
 my $pi = RPi::PIGPIO->connect('127.0.0.1');
 
 my $game = Icydee::GeoPuzzle->new({ fsa_state => 'ping', pi => $pi });
+
+my $hc595 = Icydee::HC595->new({
+    pi  => $pi,
+});
+
+
+
+
+while (1) {
+    foreach my $output (0..255) {
+        my $t0 = [gettimeofday];
+        $hc595->output([$output]);
+        my $elapsed = tv_interval($t0, [gettimeofday]);
+        print "Elapsed: $elapsed\n";
+        sleep(1);
+    }
+}
+
 
 while ($game->fsa_state ne 'end') {
     $game->fsa_check_state;
--- lib/Icydee/HC595.pm
+++ lib/Icydee/HC595.pm
@@ -1,10 +1,10 @@
 package Icydee::HC595;
 
 use Moose;
-use RPi::PIGPIO;
+use RPi::PIGPIO ':all';
 use Time::HiRes qw(usleep);
 
-# Raspberry pi pin connected to Register Clock
+# Raspberry pi pin connected to Register Clock (chip pin 12)
 #
 has rclk_pin => (
     is      => 'rw',
@@ -12,14 +12,14 @@ has rclk_pin => (
     default => 4,
 );
 
-# Raspberry pi pin connected to Shift Register Clock
+# Raspberry pi pin connected to Shift Register Clock (chip pin 11)
 has srclk_pin => (
     is      => 'rw',
     isa     => 'Int',
     default => 5,
 );
 
-# Raspberry pi pin connected to Serial Data
+# Raspberry pi pin connected to Serial Data (chip pin 14)
 #
 has ser_pin => (
     is      => 'rw',
@@ -30,15 +30,15 @@ has ser_pin => (
 has pi => (
     is      => 'rw',
     default => sub {},
-};
+);
 
 
 sub BUILD {
     my ($self) = @_;
 
-    foreach my $pin (qw(clock_pin serial_pin latch_pin)) {
+    foreach my $pin (qw(rclk_pin srclk_pin ser_pin)) {
         $self->pi->set_mode($self->$pin, PI_OUTPUT);
-        $self->pi->write($self->$pin, LO);
+        $self->pi->write($self->$pin, LOW);
     }
 }
 
@@ -49,8 +49,18 @@ sub rclk_pulse {
     my ($self) = @_;
 
     $self->pi->write($self->rclk_pin, HI);
-    usleep(1000);
-    $self->pi->write($self->rclk_pin, LO);
+    usleep(1);
+    $self->pi->write($self->rclk_pin, LOW);
+    usleep(1);
+}
+
+sub srclk_pulse {
+    my ($self) = @_;
+
+    $self->pi->write($self->srclk_pin, HI);
+    usleep(1);
+    $self->pi->write($self->srclk_pin, LOW);
+    usleep(1);
 }
 
 # Output the data, one byte per device
@@ -61,10 +71,9 @@ sub output {
 
     foreach my $byte (@$bytes) {
         foreach my $bit (0..7) {
-            $self->pi->write($self->ser_pin, 0x80 & ($byte << $bit);
-            $self->pi->write($self->srclk, HI);
-            usleep(1000);
-            $self->pi->write($self->srclk, LO);
+            my $state = 0x80 & ($byte << $bit) ? HI : LOW;
+            $self->pi->write($self->ser_pin, $state);
+            $self->srclk_pulse;
         }
     }
     $self->rclk_pulse;
